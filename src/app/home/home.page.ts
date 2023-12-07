ff02cec06bae3c8e7821a8a1d3daf3e94c01cff0
msuito <matiassuito@gmail.com>
2023-12-07 02:35:24 -0300

    Nuevo diseño de páginas e imagenes + logout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,4 +18,7 @@ export class HomePage {
 
     });
   }
+  cerrarsesion() {
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/homeprof/homeprof.page.ts
+++ src/app/homeprof/homeprof.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/autenticacion.service';
 @Component({
   selector: 'app-homeprof',
   templateUrl: './homeprof.page.html',
   styleUrls: ['./homeprof.page.scss'],
 })
-export class HomeprofPage implements OnInit {
-  
-  constructor() { }
+export class HomeprofPage {
+  state: any;
+  user: any;
+  constructor(private activeroute: ActivatedRoute, private router: Router, private authService: AuthService) {
+    this.activeroute.queryParams.subscribe(params => {
+      this.state = this.router.getCurrentNavigation()?.extras.state;
+      this.user = this.state.user;
 
-  ngOnInit() {
+      if (!this.authService.isAuthenticated) {
+        this.router.navigate(['/login']);
+      }
+    });
   }
-
+  cerrarsesion() {
+    this.router.navigate(['/login']);
+  }
+  generarqr() {
+    this.router.navigate(['/mostrarqr']);
+  }
 }

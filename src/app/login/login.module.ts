02e780013dae9189fd485cee781e5fa2a3e5d4b7
Mur <deltamur@hotmail.com>
2023-12-07 01:06:30 -0300

    login y routing arreglado

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -24,24 +24,3 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 })
 export class LoginPageModule {}
 
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
-import { LoginPageRoutingModule } from './login-routing.module';
-
-import { LoginPage } from './login.page';
-import { Router } from '@angular/router';
-
-@NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    LoginPageRoutingModule
-  ],
-  declarations: [LoginPage]
-})
-export class LoginPageModule {}
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -100,37 +100,8 @@ export class LoginPage {
       // Redirigir a una página específica para correos con dominio "duoc.cl"
       this.router.navigate(['/home']);
     }
-    else if(dominio==='profesor.duoc.cl'){
-      this.router.navigate(['/home-profesor']);
+    else if(dominio==='profesor.cl'){
+      this.router.navigate(['/homeprof']);
     }  
   }
-}
-import { Component } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
-import { AuthService } from 'src/app/autenticacion.service';
-
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss']
-})
-export class LoginPage {
-  user = {
-    usuario: "",
-    password: ""
-  };
-
-  constructor(private router: Router, private authService: AuthService) {}
-
-  login() {
-    if (this.authService.login(this.user.usuario, this.user.password)) {
-      let navigationExtras: NavigationExtras = {
-        state: { user: this.user }
-      };
-      this.router.navigate(['/home'], navigationExtras);
-    } else {
-      this.router.navigate(['/login']);
-    }
-  }
-
 }
